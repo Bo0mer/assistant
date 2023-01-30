dd0e24b617e75b37977ac8c9882dd7f2ff87f583
Ivan Borshukov <iborshukov@plandelta.com>
2023-01-30 21:12:40 +0200

    Add gorilla/mux dependency and use it for router

--- server/server.go
+++ server/server.go
@@ -1,20 +1,35 @@
 package server
 
 import (
-	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 // Server is the main type that implements the http.Handler interface.
 type Server struct {
+	r *mux.Router
 }
 
 // NewServer returns a new initialized server.
 func NewServer() *Server {
-	return &Server{}
+	s := &Server{}
+	r := mux.NewRouter()
+	s.r = r
+	r.Handle("/weather", http.HandlerFunc(s.handleGetWeather))
+	r.Handle("/meetings", http.HandlerFunc(s.handleGetMeetings))
+	return s
 }
 
 // ServeHTTP handles HTTP reqeusts by assisting the remote user.
 func (s *Server) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(w, "Hello, How can I assist you?")
+	s.r.ServeHTTP(w, req)
+}
+
+func (s *Server) handleGetWeather(w http.ResponseWriter, req *http.Request) {
+
+}
+
+func (s *Server) handleGetMeetings(w http.ResponseWriter, req *http.Request) {
+
 }
